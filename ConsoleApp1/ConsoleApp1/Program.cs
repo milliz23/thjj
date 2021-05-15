5e23fe63219f1547c31f9055b8f9ce95bcfbe91e
milliz23 <84192232+milliz23@users.noreply.github.com>
2021-05-15 12:22:24 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 45, -5, 12, 31, -12, 6, 58, 78, 455, 99, 32, 0, 45, 12}; 
+            int min = Max(array);
+            Console.Write("Минимальны эл.массива: " + min);
+            Console.ReadKey(true);
+        }
+        static int Max(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Count(); i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+    }
+}
